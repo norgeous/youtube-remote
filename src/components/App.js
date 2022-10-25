c14bc848e713a0e246e34df75977fc79941eecc8
norgeous <daniel@harte.me>
2022-10-25 19:10:15 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,13 @@ import { useAppContext } from '../contexts/AppContext';
 const App = () => {
   const {
     test,
+    YT
   } = useAppContext();
 
   return (
     <>
       new app {test}
+      {typeof YT}
     </>
   );
 };
