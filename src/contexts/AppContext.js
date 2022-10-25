e903be1cacc844fae8b2ba2276de9d08ccdde11c
norgeous <daniel@harte.me>
2022-10-25 18:17:07 +0100

    useYouTubeIframeApi

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -1,13 +1,17 @@
 import React, { createContext, useContext } from 'react';
 
+import useYouTubeIframeApi from '../hooks/useYoutubeIframeApi';
+
 const AppContext = createContext({});
 
 export const AppProvider = ({ children }) => {
+  const YT = useYouTubeIframeApi();
 
   return (
     <AppContext.Provider
       value={{
         test: 10,
+        YT,
       }}
     >
       {children}
---
+++ src/hooks/useYoutubeIframeApi.js
@@ -0,0 +1,83 @@
+// // 2. This code loads the IFrame Player API code asynchronously.
+// var tag = document.createElement('script');
+
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// // 3. This function creates an <iframe> (and YouTube player)
+// //    after the API code downloads.
+// var player;
+// function onYouTubeIframeAPIReady() {
+//   player = new YT.Player('player', {
+//     height: '390',
+//     width: '640',
+//     videoId: 'M7lc1UVf-VE',
+//     playerVars: {
+//       'playsinline': 1
+//     },
+//     events: {
+//       'onReady': onPlayerReady,
+//       'onStateChange': onPlayerStateChange
+//     }
+//   });
+// }
+
+// // 4. The API will call this function when the video player is ready.
+// function onPlayerReady(event) {
+//   event.target.playVideo();
+// }
+
+// // 5. The API calls this function when the player's state changes.
+// //    The function indicates that when playing a video (state=1),
+// //    the player should play for six seconds and then stop.
+// var done = false;
+// function onPlayerStateChange(event) {
+//   if (event.data == YT.PlayerState.PLAYING && !done) {
+//     setTimeout(stopVideo, 6000);
+//     done = true;
+//   }
+// }
+// function stopVideo() {
+//   player.stopVideo();
+// }
+
+
+import { useState, useEffect } from 'react';
+
+const loadYouTubeIframeApi = () => {
+  return new Promise(resolve => {
+    if (
+      typeof window.YT === "object" &&
+      typeof window.YT.ready === "function"
+    ) {
+      window.YT.ready(() => {
+        resolve(window.YT);
+      });
+      return;
+    }
+
+    const script = document.createElement("script");
+    script.src = "https://www.youtube.com/iframe_api";
+    script.async = true;
+    document.body.appendChild(script);
+
+    window.onYouTubeIframeAPIReady = () => resolve(window.YT);
+  });
+}
+
+
+
+const useYouTubeIframeApi = () => {
+  const [x,setX] = useState();
+
+  useEffect(() => {
+    const y = loadYouTubeIframeApi();
+    console.log(y);
+    setX(y);
+  }, []);
+
+  return x;
+};
+
+export default useYouTubeIframeApi;
