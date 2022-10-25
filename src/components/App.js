f6a565dbc4d5dcf31f128319f536495d5c7bb7df
norgeous <daniel@harte.me>
2022-10-25 20:31:13 +0100

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAppContext } from '../contexts/AppContext';
+
+const playlist = ['W-59sWasI98'];
 
 const App = () => {
   const {
@@ -7,10 +9,27 @@ const App = () => {
     YT
   } = useAppContext();
 
+  useEffect(() => {
+    if (!YT) return;
+    const player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: playlist[0],
+      playerVars: {
+        'playsinline': 1
+      },
+      events: {
+        // 'onReady': onPlayerReady,
+        // 'onStateChange': onPlayerStateChange
+      }
+    });
+  }, [YT]);
+
   return (
     <>
       new app {test}
       {typeof YT}
+      <div id="player"/>
     </>
   );
 };
